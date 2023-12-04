76d3b1c903cf6d61b06321aa3ed8300247d89d7b
Julien Duseyau <julien.duseyau@gmail.com>
2023-12-04 12:51:10 +0100

    Local fix

--- config/database.php
+++ config/database.php
@@ -65,18 +65,18 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('STACKHERO_MYSQL_HOST'),
+            'host' => env('DB_HOST'),
             'port' => 3306,
-            'username' => env('STACKHERO_MYSQL_USER'),
-            'password' => env('STACKHERO_MYSQL_PASSWORD'),
-            'database' => env('STACKHERO_MYSQL_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+            'database' => env('DB_DATABASE'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'sslmode' => 'require',
+            'sslmode' => env('DB_SSL'),
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
