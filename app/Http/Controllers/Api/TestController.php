07098a3c388f8d388c1b286b52c54cabf0741cf6
Julien Duseyau <julien.duseyau@gmail.com>
2023-12-05 14:44:52 +0100

    TestController

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
 
 class TestController
 {
@@ -41,72 +42,6 @@ class TestController
     public function test()
     {
 
-//        for($i = 0; $i < 10; $i++) {
-//            dispatch(new CreateRandomCharacter());
-//        }
-
-
-        $localFiles = Storage::disk('public')->files('images');
-
-        foreach ($localFiles as $file) {
-            $contents = Storage::disk('public')->get($file);
-            $destination = 'images/' . basename($file);
-            dump('moving ' . $file . ' to ' . $destination);
-
-            Storage::disk('gcs')->put($destination, $contents);
-        }
-
-
-
-
-
-//        return response()->json([]);
-
-//        $character = Character::find(5);
-//        $description = $this->prompts->getCharacterDescription($character);
-//
-//        $response = $this->openAIRequest->sendMessage(
-//            "Summarize a short prompt, max. 1-2 sentence for Dall-E to create a portrait that fits the character and matches the image style:
-//            - {$description}
-//            Image style:
-//            - Centered Portrait, Filling the frame,
-//            - Modern Graphic Novel, Bold Contouring, Flat Colors with Gradient Shading, Limited Color Palette
-//            - Background: solid color with a subtle gradient
-//            Example prompts:
-//            - A portrait of an elderly person, capturing the wrinkles, texture of the skin, and expressive eyes that tell a story of a lifetime, in high resolution and natural lighting.
-//            - A portrait of a mysterious woman sitting in an ornate, gothic throne room, with subtle elements of fantasy and magical realism, painted with the dark and dramatic style of the Baroque period.
-//            ");
-//
-//
-//        //$this->dalle->setSize("512x512");
-//        $this->dalle->setModel("dall-e-3");
-//
-//        $response = $this->dalle->sendRequest("
-//            $response
-//        ");
-//
-//        if ($response instanceof Collection) {
-//            return response()->json($response);
-//        }
-//
-//
-//        $url = $response->url;
-//
-//        $imageContent = Http::get($url)->body();
-//
-//        $filename = Str::random(24) . '.png';
-//        $storagePath = 'images/' . $filename; // Define a path in your storage disk
-//        Storage::disk('public')->put($storagePath, $imageContent);
-//
-//        $character->image_url = $storagePath;
-//        $character->save();
-//
-//        $publicUrl = Storage::disk('public')->url($storagePath);
-//        $file = Storage::disk('public')->get($storagePath);
-//        $type = Storage::disk('public')->mimeType($storagePath);
-//
-//        return response()->json($character);
-//        return response($file, 200)->header('Content-Type', $type);
 
     }
 }
